0884b59121934a0a41db427ce7b050eb4197b6cd
tsousacorreia <tsousacorreia@outlook.com>
2024-03-04 19:31:04 -0300

    Exemplo 01 PHP

---
+++ exemplo01.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exemplo 01 - PHP</title>
+</head>
+<body>
+	<?php 
+
+		$a = 10;
+		echo "O valor de a é " . $a;	
+
+	 ?>
+</body>
+</html>
