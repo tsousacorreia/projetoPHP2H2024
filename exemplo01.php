72de08dc592f4a84839728de6dc9e01884441e1a
tsousacorreia <tsousacorreia@outlook.com>
2024-03-04 20:24:14 -0300

    Tipos de variáveis

--- exemplo01.php
+++ exemplo01.php
@@ -8,8 +8,18 @@
 <body>
 	<?php 
 
-		$a = 10;
-		echo "O valor de a é " . $a;	
+		//Estilo  de comentário de linha
+
+		/*Estilo de comentário de bloco*/
+
+		# Estilo de comentário de linha
+
+		$variavel1 = 10; //integer
+		$variavel2 = 10.5; //float
+		$variavel3 = "Texto"; //string
+		echo "O valor de variavel1 é " . $variavel1;
+		echo "O valor de variavel2 é " . $variavel2;
+		echo "O valor de variavel3 é " . $variavel3;
 
 	 ?>
 </body>
---
+++ exemplo02.php
@@ -0,0 +1,18 @@
+<?php 
+
+	//declarando variáveis e inicializando
+	$valor1 = 20;
+	$valor2 = 40.50;
+	$nome = "Etecia";
+	$cidade = 'São Paulo';
+	$flag = true;
+	$status = false;
+
+	echo "$valor1 <br />";
+	echo "$valor2 <br />";
+	echo "$nome <br />";
+	echo "$cidade <br />";
+	echo "$flag <br />";
+	echo "$status <br />";
+
+ ?>
