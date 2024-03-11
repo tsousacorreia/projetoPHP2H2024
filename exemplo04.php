44a56337201815808703a6ff4ce41604b13ab38b
tsousacorreia <tsousacorreia@outlook.com>
2024-03-11 19:49:00 -0300

    aula dia 11-03-24

---
+++ exemplo04.php
@@ -0,0 +1,14 @@
+<?php 
+
+	$valor1 = 50.00;
+	$valor2 = 30.50;
+
+	$soma = $valor1 + $valor2;
+
+	echo "A soma de $valor1 + $valor2 é $soma.<br />";
+
+	echo '<br />A soma de $valor1 + $valor2 é $soma. <br />';
+
+	echo '<br />A soma de ' . $valor1 . ' + ' . $valor2 . ' é ' . $soma . '.<br />';
+
+ ?>
