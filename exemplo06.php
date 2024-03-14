197a4a7f8c5a93956cde77b5c710f710cb8b411e
tsousacorreia <tsousacorreia@outlook.com>
2024-03-13 22:11:45 -0300

    aula dia 13-03-24

---
+++ exemplo06.php
@@ -0,0 +1,16 @@
+<?php 
+
+	$ano = 2024;
+
+	function exibir(){
+		GLOBAL $ano;
+		$ano++;
+		return $ano;
+	}
+
+	echo "<br /> Ano: ".$ano;
+	echo "<br /> Ano: ".exibir();
+	echo "<br /> Ano: ".exibir();
+	echo "<br /> Ano: ".$ano;
+
+ ?>
