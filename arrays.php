6f70149d223f32299a0ee77fa3bfdb61c57dfe29
tsousacorreia <tsousacorreia@outlook.com>
2024-04-24 22:11:32 -0300

    Arrays atualizado 24-04-2024

---
+++ arrays.php
@@ -0,0 +1,33 @@
+<?php 
+
+	/*$vetor = array();
+
+	for ($i=0; $i < 10; $i++) { 
+		$vetor[$i] = $i + 1;
+	}
+
+	foreach ($vetor as $valores) {
+		echo "<br/>" .$valores;
+	}*/
+
+
+	$nome = array('Maria', 'João', 'Pedro', 'Paulo', 'Lucas');
+
+	echo 'Os nomes no array são:<br/>';
+
+	foreach ($nome as $nomes) {
+		echo '<br/>' .$nomes;
+	}
+
+	echo "<br/><br/>";
+
+	$num = array(1, 2, 3, 4, 5);
+
+	foreach ($num as $valor) {
+		echo "Valor: $valor <br/>";
+	}
+
+	$arrayName = array('' => , );
+
+
+ ?>
