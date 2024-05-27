d77da12c5b7e81a0e2aa5f721832646f58a4b7cf
Thiago Correia <145598872+tsousacorreia@users.noreply.github.com>
2024-05-27 20:23:45 -0300

    Formulario PHP

---
+++ form.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<title>Formulário</title>
+</head>
+<body>
+
+	<form action="primeiro.php" method="post">
+		<label for="nome">Nome</label>
+		<input type="text" id="nome" name="nome" maxlength="33" placeholder="Insira o seu nome" autocomplete="true" autofocus required><br>
+		<label for="email">Email</label>
+		<input type="email" id="email" name="email" placeholder="email@email.com" autocomplete="true" required><br>
+		<label for="idade">Idade</label>
+		<input type="number" id="idade" name="idade" placeholder="0" autocomplete="true" required><br>
+
+		<br><input type="submit" name="enviar" value="Enviar">
+
+		<input type="reset" name="reset" value="Limpar">
+	</form>
+
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+
+</body>
+</html>
